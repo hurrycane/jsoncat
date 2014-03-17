0b12297afdb65e478454f6f64582cb1c64487f15
Bogdan Gaza <bc.gaza@gmail.com>
2014-03-17 13:05:56 +0200

    Silent fail and version bump to 0.1.1

--- jsoncat/runner.py
+++ jsoncat/runner.py
@@ -3,5 +3,8 @@ import json
 
 def execute_from_cli():
   for line in sys.stdin.readlines():
-    parsed = json.loads(line)
-    print json.dumps(parsed, indent=2, sort_keys=True)
+    try:
+      parsed = json.loads(line)
+      print json.dumps(parsed, indent=2, sort_keys=True)
+    except:
+      pass
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 requires = []
 
 setup(name="jsoncat",
-      version="0.1.0",
+      version="0.1.1",
       platforms='any',
       packages = find_packages(),
       include_package_data=True,
